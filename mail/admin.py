b2ad492da7ab9533d8e6dd0f8e0a967175adb74e
wer08 <wz08061992@gmail.com>
2022-10-06 14:46:54 +0200

    Working on archivisation

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from mail.models import User, Email
+
+admin.site.register(User)
+admin.site.register(Email)
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
   // Sending e-mail
   document.querySelector('#compose-submit').addEventListener('click',sent_mail);
+
   
 });
 
@@ -24,17 +25,122 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+  function get_emails(email,mailbox){
+    var div = document.createElement("div");
+    document.querySelector('#emails-view').appendChild(div);
+    div.setAttribute('class', 'border'); 
+    //clicking on e-mail will open it
+    div.addEventListener('click', function() {
+      show_email(email,mailbox);
+    });
+    if (email.read == true)
+    {
+      div.style.backgroundColor = 'silver';
+    }
+    else
+    {
+      div.style.backgroundColor = 'white';
+    }
+    div.innerHTML = `Sender: ${email.sender}  Recipients: ${email.recipients} Subject: ${email.subject}  Timestamp: ${email.timestamp} Read: ${email.read}`;
+  }
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Show e-mails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // ... do something else with emails ...
+    emails.forEach(email => get_emails(email,mailbox));
+  }
+  )
+}
+
+//Function to show clicked e-mail
+function show_email(email,mailbox)
+{
+  let mail = document.querySelector('#email');
+  mail.style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  if (mailbox == 'sent'){
+    document.querySelector('#archive').style.display = 'none';
+  }
+  else
+  {
+    document.querySelector('#archive').style.display = 'block';
+    if(email.archived == true)
+    {
+      document.querySelector('#archive').innerHTML = 'Disarchive';
+    }
+    else
+    {
+      document.querySelector('#archive').innerHTML = 'Archive';
+    }
+    
+    document.querySelector('#archive').addEventListener('click',function () {
+      if(email.archived == true)
+      {
+        
+        disarchive(email);
+        console.log(email.archived);
+      }
+      else
+      {
+        archive(email);
+        console.log(email.archived);
+      }
+
+    });
+
+  }
+  document.querySelector('#title').innerHTML = `<h2> Subject: ${email.subject}</h2>`;
+  document.querySelector('#sender').innerHTML = `<h3>${email.sender}</h3>`;
+  document.querySelector('#body').innerHTML = `${email.body}`;
+  fetch(`/emails/${email.id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+
+}
+
+//Function to archive
+function archive(email){
+  fetch(`/emails/${email.id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: true
+    })
+  })
+  document.querySelector('#archive').innerHTML = 'Disarchive';
+}
+
+//Function to disarchive
+function disarchive(email){
+  fetch(`/emails/${email.id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: false
+    })
+  })
+  document.querySelector('#archive').innerHTML = 'Archive';
 }
 
 
+//Function to send e-mail
 function sent_mail(){
   fetch('/emails', {
     method: 'POST',
